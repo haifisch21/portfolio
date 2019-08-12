62f7c28060bb2960e40f2910a53e984024a5ddd4
mrhaifisch21 <citrus29042015@icloud.com>
2019-08-12 12:34:32 +0300

    Some changes

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,11 +1,2 @@
-"use strict";
-var swiper = new Swiper(".swiper-container", {
-    autoHeight: !0,
-    spaceBetween: 30,
-    centeredSlides: !0,
-    loop: !0,
-    autoplay: {delay: 7500, disableOnInteraction: !1},
-    pagination: {el: ".swiper-pagination", clickable: !0},
-    navigation: {nextEl: ".swiper-button-next-unique", prevEl: ".swiper-button-prev-unique"}
-});
-
+"use strict";var swiper=new Swiper(".swiper-container",{autoHeight:!0,spaceBetween:30,centeredSlides:!0,loop:!0,autoplay:{delay:7500,disableOnInteraction:!1},pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next-unique",prevEl:".swiper-button-prev-unique"}});
+//# sourceMappingURL=scripts.js.map
